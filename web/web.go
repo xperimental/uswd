1621be71bfc9641be37c8bfbd8ccd6a47f6ca161
Robert Jacob <xperimental@solidproject.de>
2018-03-22 01:48:55 +0100

    Implement web handlers.

--- web/web.go
+++ web/web.go
@@ -2,7 +2,9 @@
 package web
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"strings"
 
@@ -24,19 +26,62 @@ func DatabaseHandler(database db.Database) http.Handler {
 }
 
 func handleGet(database db.Database, w http.ResponseWriter, r *http.Request) {
-	// TODO implement get
+	key := getKey(r)
+	if key == "" {
+		handleGetList(database, w, r)
+		return
+	}
+
+	handleGetSingle(database, key, w, r)
 }
 
 func handleGetList(database db.Database, w http.ResponseWriter, r *http.Request) {
-	// TODO implement get list
+	keys, err := database.List()
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Database error: %s", err), http.StatusInternalServerError)
+		return
+	}
+
+	if err := json.NewEncoder(w).Encode(keys); err != nil {
+		http.Error(w, fmt.Sprintf("Error encoding JSON: %s", err), http.StatusInternalServerError)
+		return
+	}
 }
 
 func handleGetSingle(database db.Database, key string, w http.ResponseWriter, r *http.Request) {
-	// TODO implement get single
+	content, found, err := database.Get(key)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Error getting content: %s", err), http.StatusInternalServerError)
+		return
+	}
+
+	if !found {
+		http.Error(w, fmt.Sprintf("Key not found: %s", key), http.StatusNotFound)
+		return
+	}
+
+	fmt.Fprint(w, content)
 }
 
 func handlePut(database db.Database, w http.ResponseWriter, r *http.Request) {
-	// TODO implement put
+	key := getKey(r)
+	if key == "" {
+		http.Error(w, "Key can not be empty!", http.StatusBadRequest)
+		return
+	}
+
+	content, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Error reading body: %s", err), http.StatusBadRequest)
+		return
+	}
+
+	if err := database.Put(key, string(content)); err != nil {
+		http.Error(w, fmt.Sprintf("Error writing content: %s", err), http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Fprintln(w, "saved.")
 }
 
 func getKey(r *http.Request) string {
