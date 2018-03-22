dfe63e8dea9fdd400b74941cb0a27711c4f35d5c
Robert Jacob <xperimental@solidproject.de>
2018-03-22 01:54:37 +0100

    Use file database in server.

--- cmd/uswd-server/main.go
+++ cmd/uswd-server/main.go
@@ -9,13 +9,20 @@ import (
 	"github.com/xperimental/uswd/web"
 )
 
-var addr = ":8080"
+var (
+	baseDir = "./data/"
+	addr    = ":8080"
+)
 
 func main() {
+	flag.StringVar(&baseDir, "base", baseDir, "Base directory of database.")
 	flag.StringVar(&addr, "addr", addr, "Network address to listen on.")
 	flag.Parse()
 
-	db := db.NewMemoryDatabase()
+	db, err := db.NewFileDatabase(baseDir)
+	if err != nil {
+		log.Fatalf("Error initializing database: %s", err)
+	}
 
 	http.Handle("/", web.DatabaseHandler(db))
 
