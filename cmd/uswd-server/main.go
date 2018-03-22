c57e5fde94be12a3d2008320ac85596600edeaf0
Robert Jacob <xperimental@solidproject.de>
2018-03-22 01:10:35 +0100

    Add Hello service.

---
+++ cmd/uswd-server/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", helloHandler)
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello HTTP!")
+}
